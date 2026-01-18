eaeb943eb28f461f7f50021e73bfd2f131fa9ded
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-01-18 14:56:45 +0900

    qip

--- products/frontend/src/routes/Root/components/index.ts
+++ products/frontend/src/routes/Root/components/index.ts
@@ -1,4 +1,5 @@
 export { default as DeleteHistory } from './DeleteHistory/DeleteHistory';
 export { default as HistoryForm } from './HistoryForm/HistoryForm';
 export { default as History } from './History/History';
-export { default as ReminderTest } from './ReminderTest/ReminderTest.tsx'
+export { default as ReminderTest } from './ReminderTest/ReminderTest';
+export { default as WarningMessage } from './WarningMessage/WarningMessage';
